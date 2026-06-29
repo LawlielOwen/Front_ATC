416178a06ab7c180ae9b9ec70beae72561c20734
Morgan <Karypekito@gmail.com>
2026-06-28 23:40:25 -0600

    dsa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiurl: 'https://back-atc.onrender.com/api' // ¡Asegúrate de cambiar esto!
+  // apiurl: 'https://back-atc.onrender.com/api' // ¡Asegúrate de cambiar esto!
+  apiurl:'http://192.168.1.71:3000/api'
 };
